bf993e5f7eb3c4a1f79fd515e21b8d2741877717
george udosen <datameshprojects@gmail.com>
2016-07-08 15:44:35 +0100

    template finished

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -2,8 +2,64 @@
 
 using namespace std;
 
+
+template<class T, class U> T smaller(T a, U b){
+
+    return (a < b ? a : b);
+
+};
+
+template<class T> class MyClass{
+
+    public:
+        MyClass(T x){
+            cout << x << "- not a char" << endl;
+
+        }
+
+
+};
+
+
+template< > class MyClass<char>{
+
+    public:
+        MyClass(char x){
+            cout << x << "is a char" << endl;
+        }
+
+};
+
+template<class T> class Pair{
+
+    public:
+        Pair(T a, T b):first(a), second(b){
+
+
+        }
+        T bigger();
+
+
+    private:
+        T first, second;
+
+
+};
+
+template<class T> T Pair<T>::bigger(){
+
+    //some code
+    return(first>second ? first : second);
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Pair<int> obj(3, 4);
+    cout << obj.bigger() << endl;
+
+    Pair<double> obj2(12.34, 23.12);
+    cout << obj2.bigger();
+
     return 0;
 }
